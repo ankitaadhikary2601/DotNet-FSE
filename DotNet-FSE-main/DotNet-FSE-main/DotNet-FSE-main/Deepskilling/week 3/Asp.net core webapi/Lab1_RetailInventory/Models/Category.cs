c8d108875b658734b441fc3256e7304976ca6f4f
ankita_adhikary <ankita.adhikary2601@gmail.com>
2026-07-17 20:06:57 +0530

    Delete DotNet-FSE-main/DotNet-FSE-main/DotNet-FSE-main/Deepskilling/week 3/Asp.net core webapi directory

--- DotNet-FSE-main/DotNet-FSE-main/DotNet-FSE-main/Deepskilling/week 3/Asp.net core webapi/Lab1_RetailInventory/Models/Category.cs
+++
@@ -1,8 +0,0 @@
-namespace Lab1_RetailInventory.Models
-{
-    public class Category
-    {
-        public int CategoryId { get; set; }
-        public string Name { get; set; } = "";
-    }
-}
--- DotNet-FSE-main/DotNet-FSE-main/DotNet-FSE-main/Deepskilling/week 3/Asp.net core webapi/Lab2_DatabaseContext/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using Lab2_DatabaseContext.Data;
-
-Console.WriteLine("Lab 2: Setting Up the Database Context");
-
-using var db = new AppDbContext();
-
-Console.WriteLine("Database Context Configured Successfully!");
